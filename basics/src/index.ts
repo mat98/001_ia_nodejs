e02813b8077b27448a797bb7d24eba2743cb55ff
mateus.santana <mateus.santana@7comm.com.br>
2025-03-07 15:33:09 -0300

    [FEAT] - Inserção da tipagem e conversor de texto em tokens

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,8 +1,10 @@
 import { OpenAI } from 'openai'
+import { encoding_for_model } from 'tiktoken'
 import 'dotenv/config'
 
 const openai = new OpenAI(
     {
+        baseURL: 'https://api.deepseek.com',
         apiKey: process.env.OPENAI_API_KEY
     }
 )
@@ -10,7 +12,7 @@ const openai = new OpenAI(
 async function main() {
     console.log(process.env.OPENAI_API_KEY)
     const response = await openai.chat.completions.create({
-        model: 'gpt-4o',
+        model: 'deepseek-chat',
         messages: [{
             role: 'user',
             content: 'How tall is mount Everest?'
@@ -20,4 +22,12 @@ async function main() {
     console.log(response.choices[0].message.content)
 }
 
-main();
+function encondePrompt() {
+    const prompt = "How are you today?"
+    const encoder = encoding_for_model('gpt-3.5-turbo');
+    const words = encoder.encode(prompt);
+    console.log(words);
+    // RETORNO => Uint32Array(5) [ 4438, 527, 499, 3432, 30 ]
+}
+
+encondePrompt();
